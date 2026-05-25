e6acf8aec71e007468ced1f12e379a5351e76ac7
Miguel Rios <miguel.rios1997@gmail.com>
2026-05-25 12:51:47 -0700

    Fix submodule and add restaurants project

---
+++ restaurants/astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import playformInline from "@playform/inline";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://miguelrios0517.github.io",
+  base: "/restaurant",
+  // trailingSlash: 'always',
+  integrations: [
+    alpinejs(),
+    mdx(),
+    (await import("@playform/inline")).default(),
+  ],
+  output: "static",
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
